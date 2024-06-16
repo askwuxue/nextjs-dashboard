86d1e57780f755c04f28f0deacb88e8981d4a511
wuxue <wuxue@finchina.com>
2024-06-16 10:51:21 +0800

    feat: improving accessibility

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -7,18 +7,41 @@ import { redirect } from 'next/navigation';
  
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+// This is temporary until @types/react-dom is updated
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
  
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = Object.fromEntries<any>(formData);
+export async function createInvoice(prevState: State, formData: FormData) {
+  const { customerId: customerIdOrigin, amount: amountOrigin, status: statusOrigin } = Object.fromEntries<any>(formData);
+  const validatedFields = CreateInvoice.safeParse({ customerId: customerIdOrigin, amount: amountOrigin, status: statusOrigin });
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+  const  { customerId, amount, status } = validatedFields.data;
   try {
-    CreateInvoice.parse({ customerId, amount, status });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
    
@@ -26,13 +49,15 @@ export async function createInvoice(formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-  
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
   } catch(error) {
-    console.log('error: ', error);
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+    // console.log('error: ', error);
     // throw new Error('Failed to create invoice.');
   }
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
